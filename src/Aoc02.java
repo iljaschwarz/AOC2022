26a5a303b76d5f1beff6a8263c8e7e01817f6ff2
Ilja Schwarz <ilja.schwarz@rtl.de>
2022-12-01 19:32:11 +0100

    prep Day 2

---
+++ src/Aoc02.java
@@ -0,0 +1,18 @@
+public class Aoc02 {
+
+    public static void main(String[] args) {
+        String input = Utils.getInput("inputs/aoc02Input.txt");
+
+        firstPart(input);
+        secondPart(input);
+
+    }
+
+
+    private static void firstPart(String input) {
+    }
+
+    private static void secondPart(String input) {
+    }
+
+}
