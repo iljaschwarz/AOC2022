5ab80f27d5202f054accabbdc6e93f0f0a1c95c2
Ilja Schwarz <ilja.schwarz@rtl.de>
2022-12-02 06:25:57 +0100

    Day 2

--- src/Aoc02.java
+++ src/Aoc02.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Aoc02 {
 
     public static void main(String[] args) {
@@ -8,11 +10,47 @@ public class Aoc02 {
 
     }
 
+    public static HashMap<String, Integer> mapFirst = new HashMap<>() {{
+        put("A X", 4);
+        put("A Y", 8);
+        put("A Z", 3);
+        put("B X", 1);
+        put("B Y", 5);
+        put("B Z", 9);
+        put("C X", 7);
+        put("C Y", 2);
+        put("C Z", 6);
+    }};
+
+    public static HashMap<String, Integer> mapSecond = new HashMap<>() {{
+        put("A X", 3);
+        put("A Y", 4);
+        put("A Z", 8);
+        put("B X", 1);
+        put("B Y", 5);
+        put("B Z", 9);
+        put("C X", 2);
+        put("C Y", 6);
+        put("C Z", 7);
+    }};
 
     private static void firstPart(String input) {
+        String[] lines = input.split("\n");
+        Integer score = 0;
+        for (String line : lines) {
+            score += mapFirst.get(line);
+        }
+        System.out.println(score);
     }
 
     private static void secondPart(String input) {
+        String[] lines = input.split("\n");
+        Integer score = 0;
+        for (String line : lines) {
+            score += mapSecond.get(line);
+        }
+        System.out.println(score);
+
     }
 
 }
