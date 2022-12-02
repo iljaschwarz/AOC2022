8d68cd702dae1e73b2b90e2da02a2f135f1872e7
Ilja Schwarz <ilja.schwarz@rtl.de>
2022-12-02 06:29:28 +0100

    clean up Day 1

--- src/Aoc01.java
+++ src/Aoc01.java
@@ -12,36 +12,31 @@ public class Aoc01 {
     }
 
     private static void firstPart(String input) {
-        ArrayList<Integer> elvs = new ArrayList<>();
-        String[] lines = input.split("\n");
-        Integer current = 0;
-        for (int i = 0; i < lines.length; i++) {
-            if (lines[i].isEmpty()) {
-                elvs.add(current);
-                current = 0;
-                continue;
-            }
-            current += Integer.parseInt(lines[i]);
-        }
-        Collections.sort(elvs, Collections.reverseOrder());
-        System.out.println(elvs.get(0));
+        ArrayList<Integer> elves = getCalories(input);
+        System.out.println(elves.get(0));
     }
 
     private static void secondPart(String input) {
-        ArrayList<Integer> elvs = new ArrayList<>();
+
+        ArrayList<Integer> elves = getCalories(input);
+
+        System.out.println(elves.get(0) + elves.get(1) + elves.get(2));
+    }
+
+    private static ArrayList<Integer> getCalories(String input) {
+        ArrayList<Integer> elves = new ArrayList<>();
         String[] lines = input.split("\n");
-        Integer current = 0;
-        for (int i = 0; i < lines.length; i++) {
-            if (lines[i].isEmpty()) {
-                elvs.add(current);
+        int current = 0;
+        for (String line : lines) {
+            if (line.isEmpty()) {
+                elves.add(current);
                 current = 0;
                 continue;
             }
-            current += Integer.parseInt(lines[i]);
+            current += Integer.parseInt(line);
         }
-        Collections.sort(elvs, Collections.reverseOrder());
-
-        System.out.println(elvs.get(0) + elvs.get(1) + elvs.get(2));
+        elves.sort(Collections.reverseOrder());
+        return elves;
     }
 
 
