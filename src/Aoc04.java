f30ed2509dacf04f56fb973a198223539b18e71c
Ilja Schwarz <ilja.schwarz@rtl.de>
2022-12-04 06:19:37 +0100

    Day 4

--- src/Aoc04.java
+++ src/Aoc04.java
@@ -9,10 +9,48 @@ public class Aoc04 {
 
 
     private static void firstPart(String input) {
+        Integer result = 0;
+        String[] lines = input.split("\n");
 
+        for (String line : lines) {
+            String[] currentLine = line.split(",");
+
+            String[] splitLeft = currentLine[0].split("-");
+            String[] splitRight = currentLine[1].split("-");
+
+            int startLeft = Integer.parseInt(splitLeft[0]);
+            int endLeft = Integer.parseInt(splitLeft[1]);
+
+            int startRight = Integer.parseInt(splitRight[0]);
+            int endRight = Integer.parseInt(splitRight[1]);
+
+            if ((startLeft <= startRight && endLeft >= endRight) || (startRight <= startLeft && endRight >= endLeft)) {
+                result += 1;
+            }
+        }
+        System.out.println(result);
     }
 
     private static void secondPart(String input) {
+        Integer result = 0;
 
+        String[] lines = input.split("\n");
+
+        for (String line : lines) {
+            String[] currentLine = line.split(",");
+            String[] splitLeft = currentLine[0].split("-");
+            String[] splitRight = currentLine[1].split("-");
+
+            int startLeft = Integer.parseInt(splitLeft[0]);
+            int endLeft = Integer.parseInt(splitLeft[1]);
+
+            int startRight = Integer.parseInt(splitRight[0]);
+            int endRight = Integer.parseInt(splitRight[1]);
+
+            if ((startLeft <= startRight && endLeft >= startRight) || (startRight <= startLeft && endRight >= startLeft)) {
+                result += 1;
+            }
+        }
+        System.out.println(result);
     }
 }
