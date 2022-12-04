5338fce92197d5f5a2e764ed162cd5bbe0be39e4
Ilja Schwarz <ilja.schwarz@rtl.de>
2022-12-04 06:41:05 +0100

    prep Day 5

---
+++ src/Aoc05.java
@@ -0,0 +1,20 @@
+public class Aoc05 {
+    public static void main(String[] args) {
+        String input = Utils.getInput("inputs/aoc04Input.txt");
+
+        firstPart(input);
+        secondPart(input);
+
+    }
+
+
+    private static void firstPart(String input) {
+        String[] lines = input.split("\n");
+
+    }
+
+    private static void secondPart(String input) {
+        String[] lines = input.split("\n");
+
+    }
+}
