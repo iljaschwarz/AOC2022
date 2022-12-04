11fdc4131c32c98fbf1fa873873188e12ca8169a
Ilja Schwarz <ilja.schwarz@rtl.de>
2022-12-04 05:45:31 +0100

    prep Day 4

---
+++ src/Aoc04.java
@@ -0,0 +1,18 @@
+public class Aoc04 {
+    public static void main(String[] args) {
+        String input = Utils.getInput("inputs/aoc04Input.txt");
+
+        firstPart(input);
+        secondPart(input);
+
+    }
+
+
+    private static void firstPart(String input) {
+
+    }
+
+    private static void secondPart(String input) {
+
+    }
+}
